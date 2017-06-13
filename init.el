ff7ecae6bfcc86e7be6aa2898784cda954ea1030
Geoffrey T. Wark <geoff@geoffwark.com>
2017-06-13 17:30:38 -0400

    Add comments and fix indent

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
+;;; init.el --- The first thing GNU Emacs runs
+
+;; Prevent Emacs clutter by moving backup/autosave files to system's tempdir
 (setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;;; init.el ends here
